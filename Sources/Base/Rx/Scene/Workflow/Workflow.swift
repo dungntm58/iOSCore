68ec10b3f862a306b042ad231e4e825de0cb66ba
dungntm58 <minhdung.uet.work@gmail.com>
2020-06-25 18:49:44 +0700

    Refactor

--- Sources/Base/Rx/Scene/Workflow/Workflow.swift
+++ Sources/Base/Rx/Scene/Workflow/Workflow.swift
@@ -7,55 +7,87 @@
 
 import RxSwift
 
-public class Workflow {
-    static var instance: Workflow = .init()
+public class Workflow  {
 
-    var currentStepObservable: Observable<AnyWorkflowStep>
+    private static var instance: Workflow = .init()
+
+    var step: Observable<AnyWorkflowStep>
     lazy var disposeBag = DisposeBag()
+    var currentScene: Scenable
 
     private init() {
-        self.currentStepObservable = .empty()
+        fatalError()
     }
 
-    private init(step: AnyWorkflowStep) {
-        self.currentStepObservable = .just(step)
+    private init<FirstStep>(step: FirstStep) where FirstStep: Launchable & WorkflowStep {
+        self.step = Observable.just(step).do(onNext: { $0.launch() }).map { $0.eraseToAny() }
+        self.currentScene = step
     }
 
-    public static func start<L>(with launchable: L) -> Workflow where L: Launchable & WorkflowStep {
-        instance = Workflow(step: AnyWorkflowStep(workflowStep: launchable))
+    public static func start<FirstStep>(firstStep: FirstStep) -> Workflow where FirstStep: Launchable & WorkflowStep {
+        instance = Workflow(step: firstStep)
         return instance
     }
 
-    public func then<WS>(handler: @escaping (_ item: Any) -> Observable<WS>) -> Workflow where WS: WorkflowStep {
-        currentStepObservable = currentStepObservable
-            .flatMap { $0.produceWorkflowItem().flatMap(handler) }
-            .map { $0.eraseToAny() }
+    public func next<WorkflowItem, NextStep>(handler: @escaping (_ previousStepItem: WorkflowItem, _ currentScene: Scenable) -> Observable<(WorkflowStepAction, NextStep)>) -> Workflow where NextStep: WorkflowStep {
+        self.step = self.step
+            .asObservable()
+            .flatMap ({ step in
+                step.produceWorkflowItem()
+                    .compactMap { $0 as? WorkflowItem }
+                    .flatMap { item in handler(item, self.currentScene) }
+                    .do(onNext: { [weak self] in self?.currentScene = step.perform(action: $0.0) })
+            })
+            .map { $0.1.eraseToAny() }
+        return self
+    }
+
+    public func next<WorkflowItem, NextStep>(handler: @escaping (_ previousStepItem: WorkflowItem, _ currentScene: Scenable) -> (WorkflowStepAction, NextStep)) -> Workflow where NextStep: WorkflowStep {
+        self.step = self.step
+            .asObservable()
+            .flatMap ({ step in
+                step.produceWorkflowItem()
+                    .compactMap { $0 as? WorkflowItem }
+                    .map { item in handler(item, self.currentScene) }
+                    .do(onNext: { [weak self] in self?.currentScene = step.perform(action: $0.0) })
+            })
+            .map { $0.1.eraseToAny() }
         return self
     }
 
     public func commit() {
-        self.currentStepObservable.subscribe().disposed(by: disposeBag)
+        step.subscribe().disposed(by: disposeBag)
     }
 }
 
-//class ExLauchableScene: Scene, Launchable, WorkflowStep {
-//    typealias WorkflowItem = Bool
-//
-//    func produceWorkflowItem() -> Observable<Bool> {
-//        .just(true)
-//    }
-//}
-//
-//class ExOtherScene: Scene, WorkflowStep {
-//    typealias WorkflowItem = Int
-//
-//    func produceWorkflowItem() -> Observable<Int> {
-//        .just(1)
-//    }
-//}
+class ExLauchableScene: Scene, Launchable, WorkflowStep {
+    typealias WorkflowItem = Bool
 
-extension ObservableConvertibleType where Element: WorkflowStep {
-    public func produce<WS>(handler: @escaping (_ item: Element.WorkflowItem) -> Observable<WS>) -> Observable<WS> where WS: WorkflowStep {
-        asObservable().flatMap { $0.produceWorkflowItem().flatMap(handler) }
+    func produceWorkflowItem() -> Observable<Bool> {
+        .just(true)
+    }
+}
+
+class ExOtherScene: Scene, WorkflowStep {
+    typealias WorkflowItem = Int
+
+    func produceWorkflowItem() -> Observable<Int> {
+        .just(1)
+    }
+}
+
+func test() {
+    Workflow
+        .start(firstStep: ExLauchableScene())
+        .next(handler: handleLaunchSceneToOtherScene(isLoggedIn:currentScene:))
+        .commit()
+}
+
+func handleLaunchSceneToOtherScene(isLoggedIn: Bool, currentScene: Scenable) -> (WorkflowStepAction, ExOtherScene) {
+    let scene = ExOtherScene()
+    if isLoggedIn {
+        return (WorkflowStepAction.switchToNewScene(scene: scene), scene)
+    } else {
+        return (WorkflowStepAction.none, scene)
     }
 }
--- Sources/Base/Rx/Scene/Workflow/WorkflowStep.swift
+++ Sources/Base/Rx/Scene/Workflow/WorkflowStep.swift
@@ -7,10 +7,74 @@
 
 import RxSwift
 
+public enum WorkflowStepAction {
+    case switchToNewScene(scene: Scenable)
+    case performChild(index: Int)
+
+    case detach
+    case detachNTimes(n: Int)
+    case detachToSceneType(type: Scenable.Type)
+    case detachToScene(scene: Scenable)
+
+    case none
+}
+
 public protocol WorkflowStep {
     associatedtype WorkflowItem
 
+    func perform(action: WorkflowStepAction) -> Scenable
     func produceWorkflowItem() -> Observable<WorkflowItem>
+}
+
+extension WorkflowStep where Self: Scenable {
+    public func perform(action: WorkflowStepAction) -> Scenable {
+        switch action {
+        case .switchToNewScene(let scene):
+            self.switch(to: scene)
+            return scene
+        case .performChild(let index):
+            self.performChild(at: index)
+            return children[index]
+        case .detach:
+            let previous = self.previous
+            self.detach()
+            return previous ?? self
+        case .detachNTimes(let n):
+            var current: Scenable? = self
+            for _ in 0..<n {
+                let previous = current?.previous
+                if previous == nil { break }
+                current = previous
+            }
+            let previous = current?.previous
+            current?.detach()
+            return previous ?? current ?? self
+        case .detachToSceneType(let t):
+            var current: Scenable? = parent
+            var isFound = false
+            while current != nil && type(of: parent) != t {
+                current = current?.parent
+                isFound = true
+            }
+            guard isFound else { return self }
+            let previous = current?.previous
+            current?.detach()
+            return previous ?? current ?? self
+        case .detachToScene(let scene):
+            var current: Scenable? = parent
+            var isFound = false
+            while parent === scene {
+                current = current?.parent
+                isFound = true
+            }
+            guard isFound else { return self }
+            let previous = current?.previous
+            current?.detach()
+            return previous ?? current ?? self
+        case .none:
+            return self
+        }
+    }
 }
 
 extension WorkflowStep {
@@ -22,6 +86,10 @@ struct AnyWorkflowStep: WorkflowStep {
 
     private let box: AnyWorkflowStepBox
 
+    @inlinable
+    var base: Any { box.base }
+
+    @usableFromInline
     init<WS>(workflowStep: WS) where WS: WorkflowStep {
         if let workflowStep = workflowStep as? AnyWorkflowStep {
             self = workflowStep
@@ -30,12 +98,20 @@ struct AnyWorkflowStep: WorkflowStep {
         }
     }
 
+    @inlinable
+    func perform(action: WorkflowStepAction) -> Scenable {
+        box.perform(action: action)
+    }
+
+    @inlinable
     func produceWorkflowItem() -> Observable<Any> {
         box.produceWorkflowItem()
     }
 }
 
 private protocol AnyWorkflowStepBox {
+    var base: Any { get }
+    func perform(action: WorkflowStepAction) -> Scenable
     func produceWorkflowItem() -> Observable<Any>
 }
 
@@ -43,10 +119,20 @@ private extension AnyWorkflowStep {
     struct Box<Base>: AnyWorkflowStepBox where Base: WorkflowStep {
         let _base: Base
 
+        @inlinable
+        var base: Any { _base }
+
+        @usableFromInline
         init(_ base: Base) {
             self._base = base
         }
 
+        @inlinable
+        func perform(action: WorkflowStepAction) -> Scenable {
+            _base.perform(action: action)
+        }
+
+        @inlinable
         func produceWorkflowItem() -> Observable<Any> {
             _base.produceWorkflowItem().map { $0 }
         }
